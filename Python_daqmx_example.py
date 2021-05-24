09159515d340a3febbf330b60e6bbfded2138611
Randy Cespedes <rcespedes27dds@gmail.com>
2021-05-23 21:51:23 -0600

    Adding RPYC example

--- Python_daqmx_example.py
+++ Python_daqmx_example.py
@@ -1,5 +1,10 @@
-import nidaqmx.system
-import nidaqmx
+import rpyc
+
+HOSTNAME = 'rscd27-Precision-T3610'
+PORT = 18861
+
+connection = rpyc.connect(HOSTNAME, PORT)
+nidaqmx = connection.root.nidmm
 
 system = nidaqmx.system.System.local()
 system.driver_version
@@ -10,3 +15,5 @@ with nidaqmx.Task() as task:
 	print("Get Measurement...")
 	task.ai_channels.add_ai_voltage_chan("PXI1Slot4/ai0")
 	task.read()
+
+
