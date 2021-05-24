ab2262ba62664de3d7609a24671174e3248ad12d
Randy Cespedes <rcespedes27dds@gmail.com>
2021-05-23 21:47:28 -0600

    Adding DAQmx example

--- Python_daqmx_example.py
+++ Python_daqmx_example.py
@@ -1,5 +1,12 @@
 import nidaqmx.system
+import nidaqmx
+
 system = nidaqmx.system.System.local()
 system.driver_version
 for device in system.devices:
-	print(device)
+	print("Detected board: -> " + str(device.product_type))
+	print("Available channels -> " + str(device.ai_physical_chans))
+with nidaqmx.Task() as task:
+	print("Get Measurement...")
+	task.ai_channels.add_ai_voltage_chan("PXI1Slot4/ai0")
+	task.read()
---
+++ rpyc_MyService_nidaqmx.py
@@ -0,0 +1,11 @@
+import rpyc
+import nidaqmx
+
+class MyService(rpyc.Service):
+   exposed_nidmm = nidaqmx
+
+if __name__ == "__main__":
+	from rpyc.utils.server import ThreadedServer
+	t = ThreadedServer(MyService, port = 18861, protocol_config = {"allow_public_attrs" : True, "allow_all_attrs" : True})
+	print("Exporting NI-DAQmx...")
+	t.start()
