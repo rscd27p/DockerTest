3d8245c9f302e60d523b0a8d704f2278bc3ff92f
Randy Cespedes <rcespedes27dds@gmail.com>
2021-05-26 16:39:05 -0600

    Adding rpyc server for NI-SWITCH, NI-DMM, and NI-DCPower

--- Python_daqmx_example.py
+++ Python_daqmx_example.py
@@ -1,19 +1,19 @@
-import rpyc
+#import rpyc
+import nidaqmx
+#HOSTNAME = 'rscd27-Precision-T3610'
+#PORT = 18861
 
-HOSTNAME = 'rscd27-Precision-T3610'
-PORT = 18861
-
-connection = rpyc.connect(HOSTNAME, PORT)
-nidaqmx = connection.root.nidaqmx
+#connection = rpyc.connect(HOSTNAME, PORT)
+#nidaqmx = connection.root.nidaqmx
 
 system = nidaqmx.system.System.local()
 system.driver_version
 for device in system.devices:
 	print("Detected board: -> " + str(device.product_type))
 	print("Available channels -> " + str(device.ai_physical_chans))
-with nidaqmx.Task() as task:
+'''with nidaqmx.Task() as task:
 	print("Get Measurement...")
 	task.ai_channels.add_ai_voltage_chan("PXI1Slot4/ai0")
 	task.read()
-
+'''
 
--- rpyc_NI-SWITCH_NI-DCPower_NI-DMM.py
+++ rpyc_NI-SWITCH_NI-DCPower_NI-DMM.py
@@ -2,6 +2,7 @@
 import rpyc # python3 -m pip install rpyc
 import nidcpower # python3 -m pip install dcpower
 import niswitch # python3 -m pip install niswitch
+import nidmm #  python3 -m pip install nidmm
 
 # Import rpyc thread server
 from rpyc.utils.server import ThreadedServer
@@ -9,20 +10,31 @@ from rpyc.utils.server import ThreadedServer
 # Declare NI-SWITCH rpyc service
 class NI_SWTICH_service(rpyc.Service):
    exposed_niswitch = niswitch
+   print("NI-SWITCH thread started...")
 
 # Declare NI-DCPower rpyc service
 class NI_DCPower_service(rpyc.Service):
    exposed_nidcpower = nidcpower
+   print("NI-DCPower thread started...")
+   
+# Declare NI-DCPower rpyc service
+class NI_DMM_service(rpyc.Service):
+   exposed_nidmm = nidmm
+   print("NI-DMM thread started...")
 
 # Declare Ports
 NI_SWTICH_port = 18861
 NI_DCPower_port = 18862
+NI_DMM_port = 18863
 
 # Main code
 if __name__ == "__main__":
 	print("Starting NI-SWITCH and NI-DCPower rpyc service")
+	print("Press Ctrl+C to stop this service")
 	t1 = ThreadedServer(NI_SWTICH_service, port = NI_SWTICH_port , protocol_config = {"allow_public_attrs" : True, "allow_all_attrs" : True})
 	t2 = ThreadedServer(NI_DCPower_service, port = NI_DCPower_port, protocol_config = {"allow_public_attrs" : True, "allow_all_attrs" : True})
+	t3 = ThreadedServer(NI_DMM_service, port = NI_DMM_port, protocol_config = {"allow_public_attrs" : True, "allow_all_attrs" : True})
 	t1.start()
 	t2.start()
+	t3.start()
 	print("Press Ctrl+C to stop this service")
